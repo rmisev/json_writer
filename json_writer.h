41a033e6912d249f4be1d48e1ed6d406eba8e87a
Rimas Misevičius <rmisev3@gmail.com>
2017-05-01 21:05:55 +0300

    json_writer class

---
+++ json_writer.h
@@ -0,0 +1,216 @@
+#ifndef JSON_WRITER_H
+#define JSON_WRITER_H
+
+#include <cstdint>
+#include <cstring>
+#include <ostream>
+#include <type_traits>
+
+
+class json_writer {
+public:
+	json_writer(std::ostream& sout);
+
+	// object start / end
+	void array_start();
+	void array_end();
+	void object_start();
+	void object_end();
+
+	// write name
+	void name(const char* first, const char* last);
+	void name(const char* strz);
+
+	// write value
+	void value(const char* first, const char* last);
+	void value(const char* strz);
+	void value_bool(bool b);
+	void value_null();
+
+	// numbers
+	template<class T, typename = std::enable_if<std::is_integral<T>::value>::type>
+	inline void value(T n) {
+		value_sep_set();
+		sout_ << n;
+	}
+
+	// string support
+	template <class StringT, typename = std::enable_if<std::is_class<StringT>::value>::type>
+	void name(const StringT& str) {
+		name(str.data(), str.data() + str.size());
+	}
+	template <class StringT, typename = std::enable_if<std::is_class<StringT>::value>::type>
+	void value(const StringT& str) {
+		value(str.data(), str.data() + str.size());
+	}
+
+protected:
+	void value_sep();
+	void value_sep_set();
+
+	void write_string(const char* first, const char* last);
+	
+private:
+	std::ostream& sout_;
+	bool need_value_sep_;
+};
+
+// json_writer inline
+
+namespace {
+
+static const char cObjStart = '{';
+static const char cObjEnd   = '}';
+static const char cArrStart = '[';
+static const char cArrEnd   = ']';
+static const char cNameSep  = ':';
+static const char cValueSep = ',';
+
+}
+
+inline json_writer::json_writer(std::ostream& sout)
+	: sout_(sout)
+	, need_value_sep_(false)
+{}
+
+// object start / end
+
+inline void json_writer::array_start() {
+	value_sep();
+	sout_ << cArrStart;
+	need_value_sep_ = false;
+}
+
+inline void json_writer::array_end() {
+	sout_ << cArrEnd;
+	need_value_sep_ = true;
+}
+
+inline void json_writer::object_start() {
+	value_sep();
+	sout_ << cObjStart;
+	need_value_sep_ = false;
+}
+
+inline void json_writer::object_end() {
+	sout_ << cObjEnd;
+	need_value_sep_ = true;	
+}
+
+// write name
+
+inline void json_writer::name(const char* first, const char* last) {
+	value_sep();
+	write_string(first, last);
+	sout_ << cNameSep;	
+	need_value_sep_ = false;
+}
+
+inline void json_writer::name(const char* strz) {
+	name(strz, strz + std::strlen(strz));	
+}
+
+// write value
+
+inline void json_writer::value(const char* first, const char* last) {
+	value_sep_set();
+	write_string(first, last);
+}
+
+inline void json_writer::value(const char* strz) {
+	value(strz, strz + std::strlen(strz));	
+}
+
+inline void json_writer::value_bool(bool b) {
+	value_sep_set();
+	sout_ << (b ? "true" : "false");
+}
+
+inline void json_writer::value_null() {
+	value_sep_set();
+	sout_ << "null";
+}
+
+// write value separator
+
+inline void json_writer::value_sep() {
+	if (need_value_sep_)
+		sout_ << cValueSep;
+}
+
+inline void json_writer::value_sep_set() {
+	if (need_value_sep_)
+		sout_ << cValueSep;
+	else
+		need_value_sep_ = true;
+}
+
+// write string
+
+namespace {
+
+static const char cQuote        = '\"';
+static const char cBackslash    = '\\';
+static const char sUnicodeEsc[] = { '\\', 'u', '0', '0' };
+
+static inline char num_to_hex(int n)
+{
+	static const char sHex[] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
+	return sHex[n];
+}
+
+}
+
+inline void json_writer::write_string(const char* first, const char* last)
+{
+	sout_ << cQuote;
+
+	const char* start = first;
+	for (const char* ps = first; ps != last; ps++)
+	{
+		const char c = *ps;
+		
+		if (c == cQuote || c == cBackslash) {	
+			sout_.write(start, ps - start);
+			sout_ << cBackslash;
+			sout_ << c;
+			start = ps + 1;
+		} else if (c <= 31 || c == 127) {
+			sout_.write(start, ps - start);
+			switch (c) {
+			case '\b':
+				sout_ << cBackslash;
+				sout_ << (char)'b';
+				break;
+			case '\f':
+				sout_ << cBackslash;
+				sout_ << (char)'f';
+				break;
+			case '\n':
+				sout_ << cBackslash;
+				sout_ << (char)'n';
+				break;
+			case '\r':
+				sout_ << cBackslash;
+				sout_ << (char)'r';
+				break;
+			case '\t':
+				sout_ << cBackslash;
+				sout_ << (char)'t';
+				break;
+			default:
+				sout_.write(sUnicodeEsc, 4);
+				sout_ << num_to_hex((c >> 4) & 0xF);
+				sout_ << num_to_hex(c & 0xF);
+				break;
+			}
+			start = ps + 1;
+		}
+	}
+	if (start != last)
+		sout_.write(start, last - start);
+
+	sout_ << cQuote;
+}
+
+#endif // JSON_WRITER_H
